6b4703dab2b7a777b608fb73333086d20170c40e
RAHUL SONAKIYA <RAHUL SONAKIYA@DESKTOP-41QBBKI>
2023-04-26 19:12:17 +0530

    first commit

---
+++ Parallel/src/test/java/com/Parallel/testscript/LoginPage.java
@@ -0,0 +1,17 @@
+package com.Parallel.testscript;
+
+import org.testng.annotations.Test;
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+import com.Parallel.generic.BaseClass;
+
+public class LoginPage extends BaseClass {
+	
+	@Test
+	public void loginPage()
+	{
+		Reporter.log("LoginPage",true);
+	}
+
+}
